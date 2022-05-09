89f040867cdadd08e062f38eccbf6882c131472b
EduardoPEMA <eduardopro00@gmail.com>
2022-05-08 19:26:05 -0500

    Remove nbproject

--- Cliente-Servidor/src/cliente/servidor/ProductosView.java
+++ Cliente-Servidor/src/cliente/servidor/ProductosView.java
@@ -25,6 +25,7 @@ public class ProductosView extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(null);
         permitirAccion();
+        
     }
 
     private boolean getEstados() {
